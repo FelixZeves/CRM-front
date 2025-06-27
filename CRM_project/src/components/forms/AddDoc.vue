355f3b39c8a61592df8aa46eb0f53c03e919ff10
Pandax-exe <mr.elisar@gmail.com>
2025-06-27 18:25:33 +0500

    fix: docs

--- CRM_project/src/components/forms/AddDoc.vue
+++ CRM_project/src/components/forms/AddDoc.vue
@@ -1,36 +1,45 @@
 <script setup>
 import { ref, computed } from 'vue'
+import { getDepartments, getFormSchema} from '@/components/Utils'
 import axios from 'axios'
 
 const props = defineProps(['visible'])
 const emit = defineEmits(['update:visible', 'update-list'])
+
+const buffOptions = ref([])
+const form = ref(getFormSchema('file'));
 
 const visible = computed({
   get: () => props.visible,
   set: val => emit('update:visible', val)
 })
 
-const form = ref({
-    title: '',
-    file: null,
-    infinitely: false,
-    spec: [],
-});
+async function send() {
+    const fd = new FormData()
 
-async function createDoc() {
-    // await axios.post('/api/user/doc', form.value)
+    fd.append('title', form.value.title)
+    fd.append('body', form.value.body)
+    fd.append('infinitely', form.value.infinitely)
+    form.value.departments.forEach(dep => fd.append('departments', dep))
+    form.value.tags.forEach(tag => fd.append('tags', tag))
+
+    await axios.post('/api/user/file', fd, {headers: {'Content-Type': 'multipart/form-data'}})
     emit('update-list')
+}
+
+async function lazyLoad() {
+    buffOptions.value = (await getDepartments()).data
 }
 </script>
 
 <template>
     <q-dialog v-model="visible" backdrop-filter="blur(4px)">
         <q-card class="text-black !rounded-[15pt] !flex !w-[90vw] !min-w-[55%]">
-            <q-form @submit="createDoc" class="bg-tile p-5 !flex flex-row w-full h-full gap-2">
+            <q-form @submit="send" class="bg-tile p-5 !flex flex-row w-full h-full gap-2">
 
                 <q-card-section class="flex flex-col w-[55%] gap-y-4">
                     <q-input
-                        label="Название дркумента"
+                        label="Название документа"
                         v-model="form.title"
                         outlined
                         type="text"
@@ -38,7 +47,7 @@ async function createDoc() {
                     />
                     <div class="flex flex-row gap-x-4">
                         <q-file
-                            v-model="form.file"
+                            v-model="form.body"
                             label="Прикрепить файлы"
                             outlined
                             hide-bottom-space=""
@@ -64,41 +73,33 @@ async function createDoc() {
                 </q-card-section>
 
                 <q-card-section class="flex flex-col gap-y-4 flex-grow">
-                    <q-select v-model="form.departments" outlined use-chips label="Отделы" hint="Чтобы файл был доступен всем, оставьте поле пустым"/>
                     <q-select
-                        v-model="form.spec"
+                        label="Отделы"
+                        emit-value
+                        map-options
+                        outlined
+                        use-chips
+                        multiple
+                        :options="buffOptions"
+                        :option-label="'title'"
+                        :option-value="'id'"
+                        v-model="form.departments"
+                        @focus="lazyLoad"
+                        hint="Чтобы файл был доступен всем, оставьте поле пустым"
+                    />
+                    <q-select
+                        v-model="form.tags"
                         outlined
                         use-chips
                         label="Направленность"
-                        :rules="[val => val.length  > 0|| 'Обязательно выберите направленность']"
+
                         />
                     <q-btn label="Создать" type="submit" color="brand-velvet"/>
                 </q-card-section>
-
-                <!-- <q-card-section class="flex flex-col w-full gap-y-4">
-                    <q-input
-                        label="Название дркумента"
-                        v-model="form.title"
-                        outlined
-                        type="text"
-                        :rules="[val => val.length >= 4 || 'Минимальная длина 4 символа', val => val.length <= 80 || 'Максимальная длина 80 символов']"
-                    />
-                    <q-file
-                        v-model="form.file"
-                        label="Прикрепить файлы"
-                        outlined
-                        bg-color="brand-wait"
-                        clearable>
-                        <template v-slot:append><q-icon name="attach_file" /></template>
-                    </q-file>
-                    <q-select v-model="form.departments" outlined use-chips label="Отделы"/>
-                    <q-select v-model="form.spec" outlined use-chips label="Направленность"/>
-                    <q-btn label="Создать" type="submit" color="brand-velvet"/>
-                </q-card-section> -->
-
             </q-form>
         </q-card>
     </q-dialog>
+    <!--:rules="[val => val.length  > 0|| 'Обязательно выберите направленность']" -->
 </template>
 
 <style scoped>
--- CRM_project/src/components/layouts/DocumentsList.vue
+++ CRM_project/src/components/layouts/DocumentsList.vue
@@ -1,24 +1,20 @@
 <script setup>
 import axios from 'axios'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import DocumentOnList from '@/components/cards/DocumentOnList.vue';
 
-const docs = ref([ {name:"Приказ 3432.15 “О привлечении учеников к патриотическим мероприятиям от 01.01.2023", type: 'Приказ', spec:'Образование', date: '12.05.2025', file: "23.11.2024 будет проводиться международная математическая олимиада “Кенгуру”."},
-                    {name:"Приказ 1111.11 “О необходимости привлечения преподавателей школы для создания технического задания к CRM системе от 16.10.2024”", type: 'Нормативный документ', spec:'Образование', date: '12.05.2025', file: "testing test"},
-                    {name:"Приказ 3432.15 “О привлечении учеников к патриотическим мероприятиям от 01.01.2023”", type: 'Приказ', spec:'Образование', date: '12.05.2025', file: "testing test"},
-                    {name:"Приказ 1111.11 “О необходимости привлечения преподавателей школы для создания технического задания к CRM системе от 16.10.2024”", type: 'Приказ', spec:'Образование', date: '12.05.2025', file: "testing test"}
-])
+const docs = ref([])
+onMounted(async () => {get()})
+
+async function get() {
+    docs.value = (await axios.get('/api/user/file')).data.data
+}
+
 </script>
 
 <template>
     <q-list class="!max-w-[90%] min-h-[500px] lg:max-h-[650px] overflow-y-auto !justify-self-center">
-        <DocumentOnList 
-        v-for="doc in docs"
-        :name="doc.name"
-        :type="doc.type"
-        :spec="doc.spec"
-        :date="doc.date"
-        :file="doc.file">
+        <DocumentOnList v-for="doc in docs" :body="doc">
         </DocumentOnList>
     </q-list>
 
